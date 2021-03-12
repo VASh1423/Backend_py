2fd67cf5fa862f7a65318dc6e65a938022a0a787
VASh <shokarev.vl@gmail.com>
2021-03-12 22:16:49 +0300

    updated

--- study-django/mysite/news/views.py
+++ study-django/mysite/news/views.py
@@ -1,20 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import News, Category
 
 def index(request):
-    news = News.objects.order_by('-created_at')
-    categories = Category.objects.all()
+    news = News.objects.all()
     context = {
         'news': news, 
-        'title': 'Список новостей',
-        'categories': categories
+        'title': 'Список новостей'
     }
     return render(request, 'news/index.html', context)
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
-    return render(request, 'news/category.html', {'news': news, 'categories': categories, 'category': category})
+    return render(request, 'news/category.html', {'news': news, 'category': category})
+
+def view_news(request, news_id):
+    # news_item = News.objects.get(pk=news_id)
+    news_item = get_object_or_404(News, pk=news_id)
+    return render(request, 'news/view_news.html', {"news_item": news_item})
+
