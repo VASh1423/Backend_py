fef49d29967f368ab2f276adf3bf067b9624cb29
VASh <shokarev.vl@gmail.com>
2021-04-15 15:55:03 +0300

    updated

--- study-django/mysite/news/views.py
+++ study-django/mysite/news/views.py
@@ -1,21 +1,55 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
 
 from .models import News, Category
 from .forms import NewsForm
 
-def index(request):
-    news = News.objects.all()
-    context = {
-        'news': news, 
-        'title': 'Список новостей'
-    }
-    return render(request, 'news/index.html', context)
+class HomeNews(ListView):
+    model = News
+    template_name = 'news/home_news_list.html'
+    context_object_name = 'news'
+    extra_context = {'title': 'Главная'}
 
-def get_category(request, category_id):
-    news = News.objects.filter(category_id=category_id)
-    category = Category.objects.get(pk=category_id)
-    return render(request, 'news/category.html', {'news': news, 'category': category})
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(is_published=True)
+
+class NewsByCategory(ListView):
+    model = News
+    template_name = 'news/home_news_list.html'
+    context_object_name = 'news'
+    allow_empty = False
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(pk=self.kwargs['category_id'])
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(category_id=self.kwargs['category_id'],is_published=True)
+
+class ViewNews(DetailView):
+    model = News
+    context_object_name = 'news_item'
+    # pk_url_kwarg = 'news_id'
+
+# def index(request):
+#     news = News.objects.all()
+#     context = {
+#         'news': news, 
+#         'title': 'Список новостей'
+#     }
+#     return render(request, 'news/index.html', context)
+
+# def get_category(request, category_id):
+#     news = News.objects.filter(category_id=category_id)
+#     category = Category.objects.get(pk=category_id)
+#     return render(request, 'news/category.html', {'news': news, 'category': category})
 
 def view_news(request, news_id):
     # news_item = News.objects.get(pk=news_id)
