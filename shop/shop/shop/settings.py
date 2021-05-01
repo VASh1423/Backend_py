c46a4a86bd8cd9c6521fe0b2d271c6cc18723963
VASh <shokarev.vl@gmail.com>
2021-05-01 18:40:58 +0300

    updated

--- shop/shop/shop/settings.py
+++ shop/shop/shop/settings.py
@@ -118,7 +118,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+import os
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILS_DIR = (
+    os.path.join(BASE_DIR, 'static_dev'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
