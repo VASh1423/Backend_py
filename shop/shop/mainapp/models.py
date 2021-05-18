b0000cfef81153d257cd840c8c4ad53951abad14
VASh <shokarev.vl@gmail.com>
2021-05-18 22:50:17 +0300

    updated

--- shop/shop/mainapp/models.py
+++ shop/shop/mainapp/models.py
@@ -11,6 +11,9 @@ import sys
 
 User = get_user_model()
 
+def get_models_for_count(*model_names):
+    return [models.Count(model_name) for model_name in model_names]
+
 def get_product_url(obj, viewname):
     ct_model = obj.__class__._meta.model_name
     return reverse(viewname, kwargs={'ct_model': ct_model, 'slug': obj.slug})
@@ -40,13 +43,34 @@ class LatestProductManager:
 class LatestProducts:
     objects = LatestProductManager()
 
+class CategoryManager(models.Manager):
+    CATEGORY_NAME_CATEGORY_COUNT = {
+        'Ноутбуки': 'notebook__count',
+        'Смартфоны': 'smartphone__count'
+    }
+
+    def get_queryset(self):
+        return super().get_queryset()
+
+    def get_categories_for_left_sidebar(self):
+        models = get_models_for_count('notebook', 'smartphone')
+        qs = list(self.get_queryset().annotate(*models))
+        data = [
+            dict(name=c.name, url=c.get_absolute_url(), count=getattr(c, self.CATEGORY_NAME_CATEGORY_COUNT[c.name])) for c in qs
+        ]
+        return data
 
 class Category(models.Model):
     name = models.CharField(max_length=255, verbose_name='Имя категории')
     slug = models.SlugField(unique=True)
+    objects = CategoryManager()
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("category_detail", kwargs={'slug': self.slug})
+    
 
 class Product(models.Model):
     MIN_RESOLUTION = (400, 400)
