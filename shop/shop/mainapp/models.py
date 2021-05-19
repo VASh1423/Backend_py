80772e0b5b055fd49d1607673dfe50a08a563b5c
VASh <shokarev.vl@gmail.com>
2021-05-19 22:49:20 +0300

    updated

--- shop/shop/mainapp/models.py
+++ shop/shop/mainapp/models.py
@@ -29,16 +29,19 @@ class LatestProductManager:
     def get_products_for_main_page(*args, **kwargs):
         with_respect_to = kwargs.get('with_respect_to')
         products = []
-        ct_models = ContentType.objects.filter(model__in)
+        ct_models = ContentType.objects.filter(model__in=args)
         for ct_model in ct_models:
             model_products = ct_model.model_class()._base_manager.all().order_by('-id')[:5]
             products.extend(model_products)
         if with_respect_to:
-            ct_model = ContentType.objects.filter(model='with_respect_to')
+            ct_model = ContentType.objects.filter(model=with_respect_to)
             if ct_model.exists():
                 if with_respect_to in args:
-                    return sorted(products, key=lambda x: x.__class__._meta.model_name.startswith(with_respect_to), reverse=True)
+                    return sorted(
+                        products, key=lambda x: x.__class__._meta.model_name.startswith(with_respect_to), reverse=True
+                    )
         return products
+
 
 class LatestProducts:
     objects = LatestProductManager()
--- shop/shop/mainapp/views.py
+++ shop/shop/mainapp/views.py
@@ -1,12 +1,21 @@
+from django.http import request
 from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Notebook, Smartphone, Category
+from django.views.generic import DetailView, View
 
-def test_view(request):
-    categories = Category.objects.get_categories_for_left_sidebar()
-    return render(request, 'base.html', {'categories': categories})
+from .models import Notebook, Smartphone, Category, LatestProducts, Customer, Cart
+from .mixins import CategoryDetailMixin
 
-class ProductDetailView(DetailView):
+class BaseView(View):
+    def get(self, request, *args, **kwargs):
+        categories = Category.objects.get_categories_for_left_sidebar()
+        products = LatestProducts.objects.get_products_for_main_page('notebook', 'smartphone', with_respect_to='smartphone')
+        context = {
+            'categories': categories,
+            'products': products
+        }
+        return render(request, 'base.html', context)
+
+class ProductDetailView(CategoryDetailMixin, DetailView):
     CT_MODEL_MODEL_CLASS = {
         'notebook': Notebook,
         'smartphone': Smartphone
@@ -23,9 +32,20 @@ class ProductDetailView(DetailView):
     template_name = 'product_detail.html'
     slug_url_kwarg = 'slug'
 
-class CategoryDetailView(DetailView):
+class CategoryDetailView(CategoryDetailMixin, DetailView):
     model = Category
     queryset = Category.objects.all()
     context_objects_name = 'category'
     template_names = 'category_detail.html'
     slug_url_kwarg = 'slug'
+
+class CartView(View):
+    def get(self, request, *args, **kwargs):
+        customer = Customer.objects.get(user=request.user)
+        cart = Cart.objects.get(owner=customer)
+        categories = Category.objects.get_categories_for_left_sidebar()
+        context = {
+            'cart': cart,
+            'categories': categories,
+        }
+        return render(request, 'cart.html', context)
