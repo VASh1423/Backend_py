ee81c241f2f3379127d7bdbe1647caed9179ec9d
VASh <shokarev.vl@gmail.com>
2021-06-25 16:26:25 +0300

    admin&header&footer

--- study-django1/siteblog/blog/models.py
+++ study-django1/siteblog/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -9,7 +10,13 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse("category", kwargs={"slug": self.slug})
+    
+
     class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
         ordering = ['title']
 
 
@@ -21,6 +28,8 @@ class Tag(models.Model):
         return self.title
     
     class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
         ordering = ['title']
 
 
@@ -39,4 +48,6 @@ class Post(models.Model):
         return self.title
 
     class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
         ordering = ['-created_at']
