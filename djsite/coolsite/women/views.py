0ede7339d0dd8b6d0a770e3bd26451465378269b
VASh <shokarev.vl@gmail.com>
2022-05-09 15:23:40 +0300

    updated

---
+++ djsite/coolsite/women/views.py
@@ -0,0 +1,20 @@
+from django.http import Http404, HttpResponse, HttpResponseNotFound
+from django.shortcuts import redirect, render
+
+def index(request):
+    return HttpResponse("Страница приложения women")
+
+def categories(request, catid):
+    if request.GET:
+        print(request.GET)
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
+
+def archive(request, year):
+    if int(year) > 2020:
+        # raise Http404()
+        return redirect('home', permanent=True)
+
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
